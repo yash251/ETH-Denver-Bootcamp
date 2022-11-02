cdbf96ae0275892a78ec5f699f9c7b24a7404d6d
Yash Jagtap <jagtapyash2512@gmail.com>
2022-11-02 16:36:15 +0530

    hw3

---
+++ Homework3/Homework3.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: None
+
+pragma solidity 0.8.17;
+
+contract BootcampContract {
+    uint256 number;
+    address deployer;
+
+    constructor() {
+        deployer = msg.sender;
+    }
+
+    function store(uint256 num) public {
+        number = num;
+    }
+
+    function retrieve() public view returns (uint256) {
+        return number;
+    }
+
+    // function getAddress() public view returns (address) {
+    //     if (msg.sender == deployer) {
+    //         return 0x000000000000000000000000000000000000dEaD;
+    //     }
+    //     return deployer;
+    // }
+
+    function getAddress() external view returns (address) {
+        return
+            deployer == msg.sender
+                ? address(0x000000000000000000000000000000000000dEaD)
+                : deployer;
+    }
+}
